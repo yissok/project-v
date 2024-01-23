2174a1709b8f1a8046e2e917a92f81951c90ef17
voltamass <voltarelmassimo@gmail.com>
2024-01-23 22:50:29 +0100

    Aggiunto Slider

--- Crosshair.gd
+++ Crosshair.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+var offset_cam = Vector3(0, 0, -1)
+
 func _ready():
 	pass
 
@@ -7,6 +9,10 @@ func _process(delta):
 	pass
 
 func moveCrossHair(direction, node, offset):
+	
 	node.rotation = node.rotation + Vector3(0, 0, 0.05 * direction)
-	var vec_dir = ($"CrossMain".position -  $"../Camera3D".position)*offset
-	node.position = vec_dir + $"../Camera3D".position + Vector3(0, 0, 1)
+	
+	offset_cam = Vector3(0,0, float($slider_offset_camera.value/-100))
+	
+	var vec_dir = ($"CrossMain".position -  ($"../Camera3D".position + offset_cam))*offset
+	node.position = vec_dir + ($"../Camera3D".position + offset_cam)
