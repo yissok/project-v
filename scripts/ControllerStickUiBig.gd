bf929ec6861e7fcfe655a7c7c33ad28c3105cb37
voltamass <voltarelmassimo@gmail.com>
2024-01-24 12:44:36 +0100

    Aggiunto indicatore stick, ambient suoni colpo e reload

---
+++ scripts/ControllerStickUiBig.gd
@@ -0,0 +1,62 @@
+extends Sprite2D
+
+var pos_y = Input.get_axis("ra_up", "ra_down")
+var pos_x = Input.get_axis("ra_left", "ra_right")
+var sparato = false
+var zona0 = false
+var zona1 = false
+var zona2 = false
+var sound1_played = false
+var sound2_played = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pos_y = Input.get_axis("ra_up", "ra_down")
+	pos_x = Input.get_axis("ra_left", "ra_right")
+	$ControllerStickUiSmall.position = Vector2(pos_x, pos_y)*200
+	_in_square(0.5, 0.5, 1, 1)
+	if (Input.is_action_pressed("shoot")==true) && (sparato == false):
+		_sparato()
+	_ricarica()
+	pass
+	
+func _in_square(x1:float, y1:float, x2:float, y2:float):
+	if (Input.get_axis("ra_up", "ra_down") >= y1) && (Input.get_axis("ra_up", "ra_down") <= y2) && (Input.get_axis("ra_left", "ra_right")>= x1)  && (Input.get_axis("ra_left", "ra_right")<= x2):
+		return true
+		
+func _sparato():
+	sparato = true
+	$gunshot.play()
+	print("Sparato")
+	pass
+		
+func _ricarica():
+	if (sparato==true) && (_in_square(-0.2, -0.2, 0.2, 0.2) == true):
+		zona0 = true
+	if (zona0== true) && (_in_square(0.6, -0.2, 1, 0.2) == true):
+		zona1 = true
+		if sound1_played == false:
+			sound1_played = true
+			$reload1.play()
+	if (zona1== true) && (_in_square(-0.2, 0.6, 0.2, 1) == true):
+		zona2 = true
+		if sound2_played == false:
+			sound2_played = true
+			$reload2.play()
+	if (zona2== true) && (_in_square(-0.2, -0.2, 0.2, 0.2) == true):
+		sparato = false
+		zona0 = false
+		zona1 = false
+		zona2 = false
+		sound1_played = false
+		sound2_played = false
+		$reload3.play()
+		print("Ricaricato")
+	pass
+	
+	
