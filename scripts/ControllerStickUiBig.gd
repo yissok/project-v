e1c1bae56f54d8e0f00f2026f8658066e558e78c
andrea <andreatv12@gmail.com>
2024-01-27 21:06:31 +0000

    quadrati di massimo

--- scripts/ControllerStickUiBig.gd
+++ scripts/ControllerStickUiBig.gd
@@ -11,15 +11,10 @@ var joy_went_down = false
 signal shot_fired
 
 enum JoystickPosition {
-	UP_LEFT,
-	UP,
-	UP_RIGHT,
-	LEFT,
 	CENTER,
 	RIGHT,
-	DOWN_LEFT,
 	DOWN,
-	DOWN_RIGHT
+	OTHER
 }
 
 func _ready():
@@ -29,34 +24,26 @@ func _process(_delta):
 	pos_x = Input.get_axis("ra_left", "ra_right")
 	pos_y = Input.get_axis("ra_up", "ra_down")
 	joystick=get_joystick_position(pos_x, pos_y)
+	print(joystick==JoystickPosition.RIGHT)
 	$ControllerStickUiSmall.position = Vector2(pos_x, pos_y)*200
 	if (Input.is_action_pressed("shoot")==true) && (sparato == false):
 		_spara()
 	_ricarica()
 	pass
 
-func get_joystick_position(x_input: int, y_input: int) -> JoystickPosition:
-	if x_input < 0:
-		if y_input < 0:
-			return JoystickPosition.UP_LEFT
-		elif y_input > 0:
-			return JoystickPosition.DOWN_LEFT
-		else:
-			return JoystickPosition.LEFT
-	elif x_input > 0:
-		if y_input < 0:
-			return JoystickPosition.UP_RIGHT
-		elif y_input > 0:
-			return JoystickPosition.DOWN_RIGHT
-		else:
-			return JoystickPosition.RIGHT
+
+func get_joystick_position(x_input: float, y_input: float) -> JoystickPosition:
+	if is_inside_square(x_input, y_input, -0.2, -0.2, 0.2, 0.2):
+		return JoystickPosition.CENTER
+	elif is_inside_square(x_input, y_input, -0.2, 0.6, 0.2, 1):
+		return JoystickPosition.DOWN
+	elif is_inside_square(x_input, y_input, 0.6, -0.2, 1, 0.2):
+		return JoystickPosition.RIGHT
 	else:
-		if y_input < 0:
-			return JoystickPosition.UP
-		elif y_input > 0:
-			return JoystickPosition.DOWN
-		else:
-			return JoystickPosition.CENTER
+		return JoystickPosition.OTHER
+
+func is_inside_square(x: float, y: float, x1: float, y1: float, x2: float, y2: float) -> bool:
+	return x >= x1 and x <= x2 and y >= y1 and y <= y2
 
 func _spara():
 	$gunshot.play()
@@ -67,8 +54,8 @@ func _spara():
 func _ricarica():
 	if (sparato==true) && (joystick == JoystickPosition.CENTER):
 		joy_state_idle = true
-		sparato = false ## WILL REMOVE THESE DW, JUST TO EASE TESTING
-		joy_went_down = false ## WILL REMOVE THESE DW, JUST TO EASE TESTING
+		#sparato = false ## WILL REMOVE THESE DW, JUST TO EASE TESTING
+		#joy_went_down = false ## WILL REMOVE THESE DW, JUST TO EASE TESTING
 	if (joy_state_idle== true) && (joystick == JoystickPosition.RIGHT):
 		joy_went_right = true
 		joy_state_idle = false
