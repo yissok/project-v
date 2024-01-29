da85e06519cb68cba8909d61fe6d8034d2dcaf29
voltamass <voltarelmassimo@gmail.com>
2024-01-29 22:18:56 +0100

    Creati 2 bus audiio diversi per musica e sfx
    Aggiunto un po' di distorsione lofi agli effetti. La musica in loop è pulita invece

--- scripts/MainScene.gd
+++ scripts/MainScene.gd
@@ -4,6 +4,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AmbienceDesert.play()
+	$AmbientePlaceholder/musica_loop.play()
 	pass # Replace with function body.
 
 
